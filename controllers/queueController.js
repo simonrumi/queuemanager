707ba3d293bd25274a72b560afb357dfd57495d4
simonrumi <simon@dsonic.com>
2018-09-13 19:03:23 -0700

    queue detail page improved

--- controllers/queueController.js
+++ controllers/queueController.js
@@ -1,11 +1,13 @@
-var QueueModel = require('../models/queueModel');
-var AttendeeModel = require('../models/attendeeModel');
-var async = require('async');
+const QueueModel = require('../models/queueModel');
+const AttendeeModel = require('../models/attendeeModel');
+const AttendeeInQueueModel = require('../models/attendeeInQueue');
+const async = require('async');
+const log = require('../logger');
 
-console.log('in queueController, required models/queueModel & models/attendeeModel');
+log('in queueController, required models/queueModel & models/attendeeModel');
 
 exports.index = function(req, res) {
-	console.log('queue.js: in index');
+	log('queue.js: in index');
 	async.parallel({
 		queueCount: function(callback) {
 			QueueModel.countDocuments({}, callback);
@@ -23,41 +25,54 @@ exports.index = function(req, res) {
 }
 
 exports.queueList = function(req, res, next) {
-	console.log('in queueList');
+	log('in queueList');
 
 	QueueModel.find({}, function(err, documents) {
 		if (err) {
-			console.log('in queueList, got an error');
+			log('in queueList, got an error');
 			res.send('had an error when listing queues: ' + err);
 		} else {
-			console.log('in queueList, about to render the queueList');
+			log('in queueList, about to render the queueList');
 			res.render('queuesView', {title: 'Queue List', queueList: documents});
 		}
 	});
 };
 
 exports.queueDetail = function(req, res) {
-	console.log('in queueDetail');
+	log('in queueDetail');
 	var queueId = req.params.id;
 	queueId = queueId.replace(':', ''); // strip out leading colon if it is there
-	QueueModel.findById(queueId, function(err, documents) {
-		res.render('queueView', {queue: documents});
+
+	async.parallel({
+		queue: function(callback) {
+			QueueModel.findById(queueId, callback);
+		},
+		attendeesInQueue: function(callback) {
+			AttendeeInQueueModel.find({'queue': queueId}).populate('attendee').exec(callback);
+		},
+	}, function(err, results) { 
+		if (err) {
+			res.render('queueView', {title: 'Queue Error Page', error: err});
+		} else {
+			log('queueDetail, results data is:    ' + JSON.stringify(results));
+			res.render('queueView', {title: 'Queue Page', error: err, data: results});
+		}
 	});
 	//res.send('Not implemented: queueDetail. Here is what is in the req<br>' + req.params.id);
 };
 
 // show a form to create a queue
 exports.queueCreateGet = function(req, res) {
-	console.log('in queueCreateGet');
+	log('in queueCreateGet');
 
 	/***** code for adding a single item 
 	var oneTestQueue = new QueueModel({'attractionName': 'Space Maountain'});
 	oneTestQueue.save(function(err) {
 		if (err) {
-			console.log('error trying to upload oneTestQueue: ' + err);
+			log('error trying to upload oneTestQueue: ' + err);
 			res.send('error trying to upload oneTestQueue: ' + err);
 		} else {
-			console.log('success: uploaded oneTestQueue');
+			log('success: uploaded oneTestQueue');
 			res.send('success: uploaded oneTestQueue');
 		}
 	});
@@ -69,6 +84,6 @@ exports.queueCreateGet = function(req, res) {
 
 // create a queue on post
 exports.queueCreatePost  = function(req, res) {
-	console.log('in queueCreatePost');
+	log('in queueCreatePost');
 	res.send('Not implemented: queueCreatePost');
 };
