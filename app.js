4b4ddc8836b083f3ff7771f8575b4dba8852cde2
simonrumi <simon@dsonic.com>
2018-10-04 21:51:10 -0700

    fixed home page routing

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', venueRouter); //indexRouter);
 app.use('/users', usersRouter);
 app.use('/venue', venueRouter);
 
