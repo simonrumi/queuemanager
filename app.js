6cd6f5852e6cc932f78faa4ca017ce9289af1e5f
simonrumi <simon@dsonic.com>
2018-10-11 10:35:26 -0700

    changing port to 443 for https

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const server = http.Server(app);
 log('server = ' + JSON.stringify(server));
 const io = require('socket.io')(server);
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 443;
 server.listen(port, function() {
 	log('server listening on port ' + port);
 });
