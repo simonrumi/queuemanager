50bcf7e05bdcadac052cf6d6c35c216820334d4d
simonrumi <simon@dsonic.com>
2018-10-13 13:49:17 -0700

    adding some css for the attendee page

--- public/javascripts/attendeeClient.js
+++ public/javascripts/attendeeClient.js
@@ -13,13 +13,24 @@ const main = function() {
 		console.log('socket io disconnected');
 	});
 
+	const joinQueueDivs = document.querySelectorAll('.joinqueue');
+	for (let i=0; i<joinQueueDivs.length; i++) {
+		let joinQueueUrl = joinQueueDivs[i].dataset.joinQueueUrl;
+		joinQueueDivs[i].onclick(function(event) {
+			console.log('got event.target.dataset.joinQueueUrl = ' + event.target.dataset.joinQueueUrl);
+			fetch(joinQueueUrl).then(function(response) {
+				console.log('heard click and got response: ' + JSON.stringify(response));
+			});
+		});
+	}
+
 	//set a cookie on the page using the attendee id from the mongo db (which is already given to us when the page )
 	setAttendeeCookie();
 }
 
 const setAttendeeCookie = (function() {
 	return function() {
-		const attendeeId = document.getElementById('attendee-info').dataset.attendeeId || 'no attendee-info tag';
+		const attendeeId = document.querySelector('#attendee-info').dataset.attendeeid || 'no attendee-info tag';
 		document.cookie = "attendeeId=" + attendeeId;
 
 		//QQQQ we do not have the attendeeId at this point - need to listen for an event from node (that we set up)  then set the cookie ...somewhow
